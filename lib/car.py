b2d783ce138db05c4da41685e8ff4eaf55d1e2f1
George Yegon <georgeyegon67@gmail.com>
2024-12-04 12:32:44 +0300

    initial commit

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,9 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        # Call the __init__ method of the parent class to set up wheel_size and wheel_number
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
